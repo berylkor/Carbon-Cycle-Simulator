21e7f12c6ab0fd618123dfaca2b1a88a67a332b8
Brakomister <96960869+Brakomister@users.noreply.github.com>
2023-04-27 19:53:37 +0000

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -1,16 +1,28 @@
+/* This class models a plant */
 public class Plant extends Living{
-    final double carbonAbsorbed;
+    final double carbonAbsorbed;// amount of carbon absorbed each time the plant photosynthesizes
 
+    //a constructor that takes a name, carbonComposition, expiredCarbon and carbonAbsorbed
     public Plant(String name,  double carbonComposition, double expiredCarbon, double carbonAbsorbed){
         super(name, "both", carbonComposition, expiredCarbon);
         this.carbonAbsorbed = carbonAbsorbed;
-        environment.addPlant(this);
+        environment.addPlant(this);// calls the addPlant method of environment
     }
 
+    /**This method gets the carbonAbsorbed
+     * 
+     * @return carbonAbsorbed
+     */
     public double getCarbonAbsorbed() {
         return carbonAbsorbed;
     }
     
+    /**This method allows the plant to photosynthesize
+     * It check if the plant is alive 
+     * If it is, it calls the decreaseCarbonContent() method of the Atmosphere class
+     * and prints the notification message
+     * Otherwise, it prints an appropriate notification message
+     */
     public void photosynthesize(){
         if (this.isAlive){
         System.out.println(this.name + " is photosynthesizing.");
@@ -20,9 +32,12 @@ public class Plant extends Living{
         }
     }
 
+    /* The plant dies
+     * 
+     */
     public void die(){
         super.die();
-        environment.removePlant(this);
+        environment.removePlant(this);// calls the removePlant() method of the Environment class
     }
 }
 
