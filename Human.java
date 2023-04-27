3da698fc1b2dc6c1d10b69d9139cc634a5d4ce9e
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2023-04-27 20:24:32 +0000

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,9 +1,16 @@
 public class Human extends Animal{
-
+    
+    // constructor that sets the name, carbon compostion, expired carbon and egested carbon
     public Human(String name,  double carbonComposition, double expiredCarbon, double egestedCarbon){
         super(name, carbonComposition, expiredCarbon, egestedCarbon);
     }
-
+    /**
+    * This method takes an object of the living class
+    * checks if Human object calling it is alive and prints out the thing the 
+    * object is burning. It then adds to the carbon content of
+    * atmosphere using the carbon compostion of whatever is being burnt
+    * else it prints a statement to show the object is dead
+    */
     public void burn(Living thing){
         if (this.isAlive){
             System.out.println(this.name + " is burning " + thing.getName());
@@ -12,7 +19,13 @@ public class Human extends Animal{
             System.out.println(this.name + " is dead and cannot burn");
         }
     }
-
+    /**
+    * This method takes a method of the living class and
+    * checks if the human class that is calling it is alive.
+    * if it is alive then it prints the statement to show
+    * that the object is recycling. If the object is dead it prints a
+    * statement to show that
+    */
     public void recycle(Living thing){
         if (this.isAlive){
             System.out.println(this.name + " recycles " + thing.getName());
