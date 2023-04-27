90866ae4b9b1a0281c48c760999bef90f1c0f3bc
Brakomister <96960869+Brakomister@users.noreply.github.com>
2023-04-27 19:50:32 +0000

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,26 +1,40 @@
+/* A class that models an animal and inherits from the Living class */
 public class Animal extends Living{
-    double egestedCarbon;
+    double egestedCarbon;// the amount of carbon released into the soil during egestion.
 
+    //constructor that takes a name, carbonCompositon, expiredCarbon and egestedCarbon
     public Animal(String name,  double carbonComposition, double expiredCarbon, double egestedCarbon){
+
         super(name, "both", carbonComposition, expiredCarbon);
         this.egestedCarbon = egestedCarbon;
-        environment.addAnimal(this);
+        environment.addAnimal(this);// calls addAnimal() method from the Environment class
     }
-
+    
+    /**
+     * This method gets the egestedCarbon
+     * @return egested Carbon
+     */
     public double getEgestedCarbon(){
         return this.egestedCarbon;
     }
+    
 
-    public void setEgestedCarbon(double egestedCarbon){
-        this.egestedCarbon = egestedCarbon;
-    }
-
+    /**
+     * Allows the animal to feed on another living 
+     * The method first checks if both prey and predator are alive
+     * If it increases the predator's carbonComposition by that of the prey
+     * It then calls the die method for the prey
+     * 
+     * If the conditions are not met, it prints out the appropriate notification messages
+     * @param prey
+     */
     public void feed(Living prey){
         if (this.isAlive && prey.isAlive){
 
             System.out.println(this.name + "  is feeding on " + prey.getName());
             this.carbonComposition += prey.getCarbonComposition();
-        
+            prey.die();
+
         } else if (!this.isAlive){
 
             System.out.println(prey.getName() + " is already dead and cannot be fed upon");
@@ -33,6 +47,11 @@ public class Animal extends Living{
         
     }
 
+    /**
+     * This messages allows the animal to egest
+     * It increases the soil's carbon content by the amount of carbon egested
+     * It then decrements the animal's carbonCOmposition by the egestedCarbon
+     */
     public void egest(){
         System.out.println(this.name + " is egested.");
         environment.soil.increaseCarbonContent(this.egestedCarbon);
@@ -41,6 +60,6 @@ public class Animal extends Living{
 
     public void die(){
         super.die();
-        environment.removeAnimal(this);
+        environment.removeAnimal(this);// calls the removeAnimal method from the environment class
     }
 }
