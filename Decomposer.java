8535877c928f20c2116637fe458a9f2e91c7c2ba
Brakomister <96960869+Brakomister@users.noreply.github.com>
2023-04-27 19:49:56 +0000

    Update Decomposer.java

--- Decomposer.java
+++ Decomposer.java
@@ -1,10 +1,15 @@
 public class Decomposer extends Living{
-    
+    //constructor that takes a name, carbonComposition and expiredCarbon
     public Decomposer(String name,  double carbonComposition, double expiredCarbon){
         super(name, "both", carbonComposition, expiredCarbon);
-        environment.addDecomposer(this);
+        environment.addDecomposer(this);//calls the addDecomposer() method from the Environment class
     }
 
+    /**
+      * This method decomposes another living thing
+      * It checks whether the thing is alive and has not been eaten
+      * It then calls the increaseCarbonContent() method from the Environment class
+     */
     public void decompose(Living thing){
         if (this.isAlive && !thing.beenEaten){
         System.out.println(this.name + " decomposes " + thing.getName());
