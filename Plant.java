423ebcfded92eb31322bf7ad52a8479ca658a5f4
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2023-04-27 20:28:38 +0000

    Update Plant.java

--- Plant.java
+++ Plant.java
@@ -1,23 +1,24 @@
 /* This class models a plant */
 public class Plant extends Living{
-    final double carbonAbsorbed;// amount of carbon absorbed each time the plant photosynthesizes
+    private final double carbonAbsorbed; // amount of carbon absorbed each time the plant photosynthesizes
 
-    //a constructor that takes a name, carbonComposition, expiredCarbon and carbonAbsorbed
+    // a constructor that takes a name, carbonComposition, expiredCarbon and carbonAbsorbed
     public Plant(String name,  double carbonComposition, double expiredCarbon, double carbonAbsorbed){
         super(name, "both", carbonComposition, expiredCarbon);
         this.carbonAbsorbed = carbonAbsorbed;
-        environment.addPlant(this);// calls the addPlant method of environment
+        environment.addPlant(this); // calls the addPlant method of environment
     }
 
-    /**This method gets the carbonAbsorbed
-     * 
+    /**
+     * This method gets the carbonAbsorbed
      * @return carbonAbsorbed
      */
     public double getCarbonAbsorbed() {
         return carbonAbsorbed;
     }
     
-    /**This method allows the plant to photosynthesize
+    /**
+     * This method allows the plant to photosynthesize
      * It check if the plant is alive 
      * If it is, it calls the decreaseCarbonContent() method of the Atmosphere class
      * and prints the notification message
@@ -32,8 +33,9 @@ public class Plant extends Living{
         }
     }
 
-    /* The plant dies
-     * 
+    /** 
+     * This method uses the parent class die method and then
+     * removes the plant from the environment using the removePlant method
      */
     public void die(){
         super.die();
